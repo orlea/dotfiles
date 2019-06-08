0101ec791fdb8c09344f0d3faa2673beb1829c2f
Aries <aries@asterism.xyz>
2019-06-08 14:21:31 +0900

    fix oneliner sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ git_pull_or_clone() {
 git_pull_or_clone https://github.com/orlea/dotfiles.git "$DOTFILES"
 
 chmod +x "$DOTFILES/_setup.sh"
-__DOTFILES="$DOTFILES" "$DOTFILES/_setup.sh"
+"$DOTFILES/_setup.sh"
 
 chmod +x "$DOTFILES/_initialize.sh"
-__DOTFILES="$DOTFILES" "$DOTFILES/_initialize.sh"
+"$DOTFILES/_initialize.sh"
