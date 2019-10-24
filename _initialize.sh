05950169d2e6a781a919c872644339792f0aa4e9
Aries <aries@asterism.xyz>
2019-10-24 16:24:36 +0900

    Change vim to nvim

--- _initialize.sh
+++ _initialize.sh
@@ -1,3 +1,3 @@
 # vim-plug install
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -c PlugInstall -c q -c q
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim -c PlugInstall -c q -c q
