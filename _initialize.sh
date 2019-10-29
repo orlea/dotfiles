22710f7ad59ab2af5372d6437dc04deb8d908b6a
Aries <aries@asterism.xyz>
2019-10-29 16:21:37 +0900

    Update _initialize.sh

--- _initialize.sh
+++ _initialize.sh
@@ -1,3 +1,4 @@
 # vim-plug install
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim -c PlugInstall -c q -c q
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim -c PlugInstall -c q -c q
