a644cc0fefd3bdb13f17927e9fbe8316e2283272
Aries <aries@asterism.xyz>
2023-09-08 14:05:30 +0900

    プラグインインストール時にシェルがしっちゃかめっちゃかになるのを修正
    たまにしか使わないしと修正してなかった

--- _initialize.sh
+++ _initialize.sh
@@ -1,4 +1,4 @@
 # vim-plug install
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +silent +VimEnter +PlugInstall +qall
+vim -E +PlugInstall +qall
